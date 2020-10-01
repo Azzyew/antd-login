7b85ca1743b9b11dcbccba0d54ae83d89227ecca
azzyew <laisadnevescosta@gmail.com>
2020-10-01 00:07:07 -0300

    adding home page

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+
+import React, { Fragment } from "react";
+
+import Content from "../components/Content";
+
+const Home = () => (
+  <Fragment>
+    <hr />
+    <Content />
+  </Fragment>
+);
+
+export default Home;
