4124e592fc3cbb055942199c7b5ef46a3bc7b4ad
azzyew <laisadnevescosta@gmail.com>
2020-10-02 10:12:11 -0300

    Adding hover to style

--- src/views/Home.js
+++ src/views/Home.js
@@ -30,25 +30,23 @@ const Home = () => {
         <Divider orientation="left" style={welcomeStyle}>Popular Movies</Divider>
           <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
             <Col className="movie-grid" span={6}>
-              <div style={style}>Enola Holmes</div>
+              <div className="movie-title" style={style}>Enola Holmes</div>
               <img src="https://images.everyeye.it/img-cover/enola-holmes-v2-42739-340.jpg" alt="enola" style={gridStyle}></img>
             </Col>
             <Col className="movie-grid" span={6}>
-              <div style={style}>O Diabo de Cada Dia</div>
+              <div className="movie-title" style={style}>O Diabo de Cada Dia</div>
               <img src="https://secureservercdn.net/50.62.172.113/48q.bb1.myftpupload.com/wp-content/uploads/2020/09/Volante-Equipment-Trouble-Form.jpg?time=1600812472" alt="devil" style={gridStyle}></img>
             </Col>
             <Col className="movie-grid" span={6}>
-              <div style={style}>Tenet</div>
+              <div className="movie-title" style={style}>Tenet</div>
               <img src="https://cdn.collider.com/wp-content/uploads/2020/07/tenet-august-release-poster-john-david-washington.jpeg" alt="tenet" style={gridStyle}></img>
             </Col>
             <Col className="movie-grid" span={6}>
-              <div style={style}>Mulan</div>
+              <div className="movie-title" style={style}>Mulan</div>
               <img src="https://image.tmdb.org/t/p/original/w1IEDHlmglRfY37AnRU60x9Lt0f.jpg" alt="mulan" style={gridStyle}></img>
             </Col>
           </Row>
             </Row>
-
-
 
     </Layout>
       
